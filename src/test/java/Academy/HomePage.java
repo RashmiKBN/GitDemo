eff97755a13f5a8bf125ae0559d35d94eb807c98
Rashmi <rashmithakbn@gmail.com>
2020-04-14 16:14:40 +0530

    modified

--- src/test/java/Academy/HomePage.java
+++ src/test/java/Academy/HomePage.java
@@ -39,6 +39,9 @@ public class HomePage extends base {
 		
 		System.out.println("---------------------------");
 		
+
+		System.out.println("---------------------------");
+		
 		Login.getsubmit().click();
 		
 	}
